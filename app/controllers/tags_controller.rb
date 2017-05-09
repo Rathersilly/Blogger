993497dd9caea82dc81921dbf6b520374efee45f
Rathersilly <rathersilly@live.com>
2017-05-08 22:16:47 -0600

    I3 complete (tags)

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -4,9 +4,16 @@ class TagsController < ApplicationController
   end
 
   def index
-
+    @tags = Tag.all
   end
 
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
 
+    flash.notice = "Tag #{@tag.name} destroyed"
+
+    redirect_to tags_path
+  end
 
 end
